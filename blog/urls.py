d3218185215dce73101f212f8eec5f17627c8ccf
Connor Rolling <connor.rolling@snhu.edu>
2022-11-10 12:46:26 -0500

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+__author__ = "Connor Rolling"
+
+from django.urls import path
+from .views import BlogListView, BlogDetailView
+
+urlpatterns = [
+    path("", BlogListView.as_view(), name='home'),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("", BlogListView.as_view(), name="home"),
+
+]
---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
